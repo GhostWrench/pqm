3326eefdab6713147dea482ebfe13c9f356d521f
Daniel <daniel.kuntz@ghostwrench.net>
2020-05-25 22:46:45 -0600

    Update packaging

--- build.bash
+++ build.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
-sed "s:export default pqm;:module.exports = pqm;:g" ./src/pqm.js > ./build/pqm-commonjs.js
-sed "s:export default pqm;::g" ./src/pqm.js > ./build/pqm-iffe.js
+cp ./src/pqm.js ./build/node/pqm.mjs
+sed "s:export default pqm;:module.exports = pqm;:g" ./src/pqm.js > ./build/node/pqm.cjs
+cp ./src/pqm.js ./build/browser/pqm.js
+sed "s:export default pqm;::g" ./src/pqm.js > ./build/browser/pqm-iife.js
