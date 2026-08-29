1c8534321a4fcfc42595e640d8b676c7701c7874
Daniel Kuntz <daniel.kuntz@ghostwrench.net>
2026-08-29 17:06:37 -0600

    Update npm modules and build script

--- build.sh
+++ build.sh
@@ -13,3 +13,4 @@ node ./node_modules/rollup/dist/bin/rollup -c
 node ./node_modules/terser/bin/terser "./build/es/pqm.mjs" -o "./build/es/pqm.min.js"
 node ./node_modules/terser/bin/terser "./build/cjs/pqm.cjs" -o "./build/cjs/pqm.min.cjs"
 node ./node_modules/terser/bin/terser "./build/iife/pqm.js" -o "./build/iife/pqm.min.js"
+
